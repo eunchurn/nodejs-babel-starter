f455f161437920a0047b2d61c4a3c413e0b12ecc
Eunchurn Park <eunchurn.park@gmail.com>
2019-10-26 08:14:39 +0900

    init commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "dotenv/config";
+import { module1 } from "@libs";
+
+const bar = module1("foo");
+console.log(bar);
---
+++ src/libs/index.js
@@ -0,0 +1,5 @@
+import "dotenv/config";
+
+export const module1 = arg => `module1 argument: ${arg}`;
+
+export const module2 = arg => `module2 argument: ${arg}`;
