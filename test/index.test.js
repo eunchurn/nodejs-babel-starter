3228313ad7b928ab5ffacb7d5b88467bdd463918
Eunchurn Park <eunchurn.park@gmail.com>
2019-10-26 08:22:51 +0900

    fix jest

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,11 @@
 import { module1, module2 } from "@libs";
 
 test("module1", () => {
-  expect(module1()).toBe("module1 argument: null");
+  expect(module1()).toBe("module1 argument: undefined");
   expect(module1("foo")).toBe("module1 argument: foo");
 });
 
 test("module2", () => {
-  expect(module2()).toBe("module2 argument: null");
+  expect(module2()).toBe("module2 argument: undefined");
   expect(module2("foo")).toBe("module2 argument: foo");
 });
